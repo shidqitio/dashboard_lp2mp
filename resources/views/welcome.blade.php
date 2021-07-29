67b37d3798f4d44535c16cd3b2bc317c49cb02d0
Muhammad Shidqi R <shidqitio@gmail.com>
2021-07-29 09:48:20 +0700

    check

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,7 +69,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Loes</a>
+                        <a href="{{ url('/home') }}">DOES</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
