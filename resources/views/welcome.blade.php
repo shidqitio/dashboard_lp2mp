5b36239de35d3287f15cdf58330f5252a29d1fbe
Muhammad Shidqi R <shidqitio@gmail.com>
2021-07-29 10:06:23 +0700

    ganti upload

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Change upload</a>
+                    <a href="https://laravel.com/docs">Change upload aja</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
